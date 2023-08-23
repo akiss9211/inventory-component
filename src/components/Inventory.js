af2133ee17065ff29a1f96cec4be95f68cc65dc8
akiss9211 <akiss9211@gmail.com>
2023-08-22 23:47:26 -0400

    inventory component coments added

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// component to render items saved in inventory state passed down by props
+// mapping it with a button and rendering the button and item name in JSX
+// button to filter out the item with the corresponding UUID saved to in its value to remove it from the rendered inventory
 function Inventory({ inventory, setInventory }) {
   return (
     <div className='inventory-container'>
